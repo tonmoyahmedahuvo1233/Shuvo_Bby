c9ea2bd538761667aff7674dbc95a885e156358d
Boss-Rahat <rahatbott10@gmail.com>
2025-09-16 14:30:27 +0600

    Update gali_reply.js

--- Script/commands/gali_reply.js
+++ Script/commands/gali_reply.js
@@ -12,13 +12,21 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Shahadat Bokasoda")==0 || event.body.indexOf("Sahadat mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("Sahadat nodir pola")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("Shahadat re chudi")==0 || event.body.indexOf("shahadat re chod")==0 || event.body.indexOf("Shahadat Abal")==0 || event.body.indexOf("Shahadat Boakachoda")==0 || event.body.indexOf("Shahadat madarchod")==0 || event.body.indexOf("Sahadat re chudi")==0 || event.body.indexOf("Sahu Bokachoda")==0) {
+	if (event.body.indexOf("Rahat Bokasoda")==0 || event.body.indexOf("রাহাদ বোকাচোদা")==0 || event.body.indexOf("বোকাচোদা")==0 || event.body.indexOf("Rahat nodir pola")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("Rahat re chudi")==0 ||
+event.body.indexOf("khanki")==0 || event.body.indexOf("murgi")==0 || event.body.indexOf("Rahat Abal")==0 ||
+event.body.indexOf("mc")==0 || event.body.indexOf("Rahat re chod")==0 || event.body.indexOf("Rahat Abal")==0 ||
+event.body.indexOf("magir pola")==0 || event.body.indexOf("chudi")==0 || event.body.indexOf("চুদি")==0 ||
+event.body.indexOf("magi")==0 || event.body.indexOf("রাহাদ কে চুদি")==0 || event.body.indexOf("Rahat ke chudi")==0 ||
+event.body.indexOf("মাগীর পোলা")==0 || event.body.indexOf("মাগী")==0 || event.body.indexOf("bal")==0 ||
+event.body.indexOf("বাল")==0 || event.body.indexOf("bokachoda")==0 || event.body.indexOf("fuck")==0 || event.body.indexOf("ফাক")==0 || event.body.indexOf("রাহাত আবাল")==0 ||
+event.body.indexOf("রাহাতরে চুদি")==0 || event.body.indexOf("চুদির ভাই")==0 || event.body.indexOf("abal")==0 ||
+event.body.indexOf("Rahat Boakachoda")==0 || event.body.indexOf("🥵")==0 || event.body.indexOf("Rahat re chudi")==0 || event.body.indexOf("🥵🥵🥵")==0) {
 		var msg = {
-				body: "তোর মতো বোকাচোদা রে আমার বস শাহাদাৎ চু*দা বাদ দিছে🤣\nসাহু এখন আর hetars চুষে না🥱😈",
+				body: "তোর মতো বোকাচোদা রে 𝗥𝗮𝗵𝗮𝘁_𝗕𝗼𝘀𝘀 চু*দা বাদ দিছে🥹🥱😈",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+	}
