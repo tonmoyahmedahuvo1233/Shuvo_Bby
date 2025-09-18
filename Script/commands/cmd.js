1e567ffbf6906c5c35fea29254a187100c56a3be
Rahat Islam <rahatbott10@gmail.com>
2025-09-18 21:00:53 +0600

    Update cmd.js

--- Script/commands/cmd.js
+++ Script/commands/cmd.js
@@ -87,7 +87,7 @@ module.exports.run = function ({ event, args, api }) {
 
  if (event.senderID != "100001039692046")
  if (event.senderID != "100089047474463")
- if (event.senderID != "61575698041722") {
+ if (event.senderID != "61561511477968") {
  return api.sendMessage("[CMD] » You are not authorized to use this command!", event.threadID, event.messageID);
  }
 
